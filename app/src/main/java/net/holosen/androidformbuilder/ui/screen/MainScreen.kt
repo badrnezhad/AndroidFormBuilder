f5494be13d8ccef493b4dada256f5c59992b9697
badrnezhad <hbadrnezhad@gmail.com>
2021-11-16 09:07:41 +0330

    ShowFab added

--- app/src/main/java/net/holosen/androidformbuilder/ui/screen/MainScreen.kt
+++ app/src/main/java/net/holosen/androidformbuilder/ui/screen/MainScreen.kt
@@ -3,7 +3,7 @@ package net.holosen.androidformbuilder.ui.screen
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -19,6 +19,7 @@ import net.holosen.androidformbuilder.viewmodels.BodyViewModel
 @Composable
 fun MainScreen(mainActivity: MainActivity) {
     val navController = rememberNavController()
+    var showFab by remember { mutableStateOf(false) }
 
     val bodyViewModel =
         ViewModelProvider(mainActivity).get(BodyViewModel::class.java)
@@ -31,17 +32,19 @@ fun MainScreen(mainActivity: MainActivity) {
             TopAppView(navController = navController)
         },
         floatingActionButton = {
-            ExtendedFloatingActionButton(
-                text = { Text(text = "New", color = PurpleGrey40) },
-                onClick = { navController.navigate("addOrEdit/-1") },
-                icon = {
-                    Icon(
-                        imageVector = Icons.Filled.Add,
-                        contentDescription = "",
-                        tint = PurpleGrey40
-                    )
-                }
-            )
+            if (showFab) {
+                ExtendedFloatingActionButton(
+                    text = { Text(text = "New", color = PurpleGrey40) },
+                    onClick = { navController.navigate("addOrEdit/-1") },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Filled.Add,
+                            contentDescription = "",
+                            tint = PurpleGrey40
+                        )
+                    }
+                )
+            }
         },
     ) {
         NavHost(
@@ -49,16 +52,19 @@ fun MainScreen(mainActivity: MainActivity) {
             startDestination = "home",
         ) {
             composable("home") {
+                showFab = true
                 HomeScreen(bodyViewModel, navController)
             }
             composable("addOrEdit/{id}",
                 arguments = listOf(
                     navArgument("id") { type = NavType.LongType }
                 )) { backStack ->
+                showFab = false
                 val id = backStack.arguments?.getLong("id")!!
                 AddOrEditScreen(id, bodyViewModel, navController)
             }
             composable("about") {
+                showFab = false
             }
         }
     }
