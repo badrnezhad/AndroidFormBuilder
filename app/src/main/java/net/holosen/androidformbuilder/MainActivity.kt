1bfcbc5a4d84fdd672038f8644c126c659e3d48b
badrnezhad <hbadrnezhad@gmail.com>
2021-11-16 08:44:03 +0330

    bodyViewModel Observer Added

--- app/src/main/java/net/holosen/androidformbuilder/MainActivity.kt
+++ app/src/main/java/net/holosen/androidformbuilder/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import net.holosen.androidformbuilder.ui.screen.MainScreen
 import net.holosen.androidformbuilder.ui.theme.AndroidFormBuilderTheme
 
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    MainScreen(this)
                 }
             }
         }
--- app/src/main/java/net/holosen/androidformbuilder/ui/screen/MainScreen.kt
+++ app/src/main/java/net/holosen/androidformbuilder/ui/screen/MainScreen.kt
@@ -5,16 +5,25 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import net.holosen.androidformbuilder.MainActivity
+import net.holosen.androidformbuilder.viewmodels.BodyViewModel
 
 @ExperimentalMaterial3Api
 @Composable
-fun MainScreen() {
+fun MainScreen(mainActivity: MainActivity) {
     val navController = rememberNavController()
+
+    val bodyViewModel =
+        ViewModelProvider(mainActivity).get(BodyViewModel::class.java)
+    bodyViewModel.getAllData().observe(mainActivity) {
+        bodyViewModel.dataList.value = it
+    }
 
     Scaffold(
         topBar = {
@@ -43,8 +52,8 @@ fun MainScreen() {
             composable("addOrEdit/{id}",
                 arguments = listOf(
                     navArgument("id") { type = NavType.LongType }
-                )) { backStack ->
-                val id = backStack.arguments?.getLong("id")!!
+                )) {
+                //val id = backStack.arguments?.getLong("id")!!
             }
         }
     }
